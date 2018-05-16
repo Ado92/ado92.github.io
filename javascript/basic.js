f613d9fc91b2ffb3e919997367cd2fdca170f01f
Andrej Hranicky <ahranicky@allplan.com>
2018-05-16 10:23:27 +0200

    Added js fix commit

--- javascript/basic.js
+++ javascript/basic.js
@@ -1,7 +1,3 @@
 $(document).ready(function(){
-    $("button").click(function(){
-        $("#div1").fadeIn();
-        $("#div2").fadeIn("slow");
-        $("#div3").fadeIn(3000);
-    });
+    $("#div2").fadeIn("slow");
 });
